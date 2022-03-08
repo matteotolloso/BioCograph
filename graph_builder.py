9c5716b297e61e1940cbe7c502ae2146212fd64b
MatteoTolloso <matteotolloso@gmail.com>
2022-03-08 22:07:55 +0100

    widest path test

--- graph_builder.py
+++ graph_builder.py
@@ -7,6 +7,61 @@ from numpy import block
 from tabulate import tabulate
 import json
 
+ 
+# Function to return the maximum weight
+# in the widest path of the given graph
+def widest_path(graph : nx.Graph, src, target):
+     
+    # To keep track of widest distance
+    widest  = {}
+    for key in graph.nodes():
+        widest[key] = -10**9
+    
+ 
+    # To get the path at the end of the algorithm
+    parent = {}
+ 
+    # Use of Minimum Priority Queue to keep track minimum
+    # widest distance vertex so far in the algorithm
+    container = []
+    container.append((src, 0))
+    container.sort(key=lambda x:x[1]) # sort on second element of tuple
+    widest[src] = 10**9
+    while (len(container)>0):
+
+        current_src = (container[-1])[0]
+        del container[-1]
+        for vertex in graph.neighbors(current_src):
+
+ 
+            # Finding the widest distance to the vertex
+            # using current_source vertex's widest distance
+            # and its widest distance so far
+            distance = max(widest[vertex], min(widest[current_src], graph[current_src][vertex]['capacity']))
+ 
+            # Relaxation of edge and adding into Priority Queue
+            if (distance > widest[vertex]):
+ 
+                # Updating bottle-neck distance
+                widest[vertex] = distance
+ 
+                # To keep track of parent
+                parent[vertex] = current_src
+ 
+                # Adding the relaxed edge in the priority queue
+                container.append((vertex, distance))
+                container.sort(key=lambda x:x[1])
+    
+    current = target
+    path = []
+    while current != src:
+        path.append(current)
+        current = parent[current]
+    path.append(src)
+
+    return path
+ 
+
 
 def build_cooccurrences_graph(  articles : dict, 
                                 mh = True, 
@@ -63,20 +118,20 @@ def build_cooccurrences_graph(  articles : dict,
                 graph.nodes[b]['weight'] += 1
             
             if graph.has_edge(a, b):
-                graph[a][b]['weight'] += 1
+                graph[a][b]['capacity'] += 1
             else:
-                graph.add_edge(a, b, weight=1)
+                graph.add_edge(a, b, capacity=1)
     
     return graph
 
 def draw(graph: nx.Graph):
    
     #layout
-    pos = nx.spring_layout(graph, seed=1)
+    pos = nx.spring_layout(graph, weight='capacity', seed=1)
     #pos = nx.spectral_layout(graph, weight='weight')
 
     #edges
-    edgewidth = [ (graph[u][v]['weight'] * 0.8) for u, v in graph.edges()]
+    edgewidth = [ (graph[u][v]['capacity'] * 0.8) for u, v in graph.edges()]
     maxi = max(edgewidth)
     edgewidth = list(map( lambda x : 50.0 * (x/float(maxi)), edgewidth))
     #nx.draw_networkx_edges(graph, pos, alpha=0.3, width=edgewidth, edge_color="m", edgelist=graph.edges(nbunch='SON'))
@@ -126,7 +181,7 @@ def main():
 
     print('Grafo delle co-occorrenze:\n\tNodi: ', len(cooccurrences_graph.nodes), '\n\tArchi: ', len(cooccurrences_graph.edges))
 
-    cooccurrences_list = list(cooccurrences_graph.edges.data('weight'))
+    cooccurrences_list = list(cooccurrences_graph.edges.data('capacity'))
     cooccurrences_list.sort(key = lambda x:x[2], reverse=True)
     with open('./results/cooccurrences.txt', 'w') as file:
             file.write(tabulate(cooccurrences_list,  headers=['Entity', 'Entity', 'Number of cooccurrences'],  tablefmt='orgtbl'))
@@ -149,6 +204,14 @@ def main():
             main_nodes.append(n)
 
     main_graph = cooccurrences_graph.subgraph(main_nodes)
+
+    src = "ZTTK"
+    dest = "NPC"
+    
+    res = widest_path(cooccurrences_graph, src, dest)
+
+    print("widest path from",src, "to", dest, ": ", res)
+
 
     draw(main_graph)
 
