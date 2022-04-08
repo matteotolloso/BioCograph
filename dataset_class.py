fb56a0e3618e6ad2119c538efd7db0ecb7bad220
MatteoTolloso <matteotolloso@gmail.com>
2022-04-08 19:56:10 +0200

    refactoring

---
+++ dataset_class.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+class Dataset:
+    def __init__(self, path : str):
+        self.name = os.path.basename(path)
+        self._papers = {}
+        with open(path, 'r') as f:
+                self._papers.update(json.loads(f.read()))
+    
+    def normalize(self, inverse_thresaurus):
+        for paper_id in self._papers:  # for each paper
+            entities_list = self._papers[paper_id]['bioBERT_entities'] # list of entities
+            normalized_entities_list = [] # new normalized list of entities
+            for entity in entities_list: # list of touple (name, type)
+                if (entity[0].lower() in inverse_thresaurus) and (entity[0].lower() not in normalized_entities_list):
+                    normalized_entities_list.append( (inverse_thresaurus[entity[0].lower()], entity[1] )    )
+                else:
+                    normalized_entities_list.append( (entity[0].lower() , entity[1]) )
+            self._papers[paper_id]['bioBERT_entities'] = normalized_entities_list
+
